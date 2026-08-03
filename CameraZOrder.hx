faa671d807be8e85a55c3773d83336808b7b1395
DubEnderDragon <101851047+DibyoExcel@users.noreply.github.com>
2026-08-03 15:38:18 +0700

    remove front option(due can be bug very top screen)

--- CameraZOrder.hx
+++ CameraZOrder.hx
@@ -4,10 +4,14 @@ import flixel.FlxCamera;
 import flixel.FlxG;
 class CameraZOrder {
     public static function moveBehind(camera:FlxCamera, targetCamera:FlxCamera):Void {
-        moveCameraOrder(camera, targetCamera, true);
-    }
-    public static function moveInFront(camera:FlxCamera, targetCamera:FlxCamera):Void {
-        moveCameraOrder(camera, targetCamera, false);
+        if (camera == null || targetCamera == null || FlxG.cameras.list.indexOf(camera) == -1 || FlxG.cameras.list.indexOf(targetCamera) == -1) return;
+        FlxG.game.removeChild(camera.flashSprite);
+        FlxG.cameras.list.remove(camera);
+        var targetIndex:Int = FlxG.game.getChildIndex(targetCamera.flashSprite);
+		var targetList = FlxG.cameras.list.indexOf(targetCamera);
+        FlxG.game.addChildAt(camera.flashSprite, targetIndex);
+        FlxG.cameras.list.insert(targetList, camera);
+        reloadIDs();
     }
     public static function moveToFront(camera:FlxCamera):Void {
         if (camera == null || FlxG.cameras.list.indexOf(camera) == -1) return;
@@ -15,21 +19,6 @@ class CameraZOrder {
         FlxG.cameras.list.remove(camera);
         FlxG.game.addChildAt(camera.flashSprite, @:privateAccess FlxG.game.getChildIndex(FlxG.game._inputContainer) + 1);
         FlxG.cameras.list.push(camera);
-        reloadIDs();
-    }
-    public static function moveCameraOrder(camera:FlxCamera, targetCamera:FlxCamera, behind:Bool = true) {
-        if (camera == null || targetCamera == null || FlxG.cameras.list.indexOf(camera) == -1 || FlxG.cameras.list.indexOf(targetCamera) == -1) return;
-        FlxG.game.removeChild(camera.flashSprite);
-        FlxG.cameras.list.remove(camera);
-        var targetIndex:Int = FlxG.game.getChildIndex(targetCamera.flashSprite);
-		var targetList = FlxG.cameras.list.indexOf(targetCamera);
-        if (behind) {
-            FlxG.game.addChildAt(camera.flashSprite, targetIndex);
-            FlxG.cameras.list.insert(targetList, camera);
-        } else {
-            FlxG.game.addChildAt(camera.flashSprite, targetIndex + 1);
-            FlxG.cameras.list.insert(targetList + 1, camera);
-        }
         reloadIDs();
     }
     public static function reorderCamera(cameraList:Array<FlxCamera>):Void {
@@ -41,7 +30,7 @@ class CameraZOrder {
         }
         for (camera in cameraList) {
             if (camera == null || camList.indexOf(camera) == -1) continue;
-            FlxG.game.addChildAt(camera.flashSprite, @:privateAccess FlxG.game.getChildIndex(FlxG.game._inputContainer) + 1);
+            FlxG.game.addChildAt(camera.flashSprite, @:privateAccess FlxG.game.getChildIndex(FlxG.game._inputContainer));
             FlxG.cameras.list.push(camera);
         }
         reloadIDs();
